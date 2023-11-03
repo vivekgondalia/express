0800545f3b39fb762a7884d3cc76512737b285f7
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-11-03 16:44:12 -0700

    Using environment variables to access server port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
 
 app.get("/api/exercises", (req, res) => {
@@ -17,4 +17,6 @@ app.get("/api/exercises", (req, res) => {
   ]);
 });
 
-app.listen(3000, () => console.log("Listening on Port 3000..."));
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port ${port}...`));
