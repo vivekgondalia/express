d6f2abbd940285db6a2dc2799d2a0a5835a1e196
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-11-03 17:03:07 -0700

    Handling HTTP Get Request

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const app = express();
 
+const exercises = [
+  { id: 1, name: "Shoulder Press", category: "Shoulder" },
+  { id: 2, name: "Side Raise", category: "Shoulder" },
+  { id: 3, name: "Front Raise", category: "Shoulder" },
+  { id: 4, name: "Shoulder Barbell", category: "Shoulder" },
+  { id: 5, name: "Suitcase Carry", category: "Shoulder" },
+  { id: 6, name: "Side Plank", category: "Shoulder" },
+  { id: 7, name: "Bird Dog", category: "Shoulder" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/api/exercises", (req, res) => {
-  res.send([
-    "Shoulder Press",
-    "Side Raise",
-    "Front Raise",
-    "Shoulder Barbell",
-    "Suitcase Carry",
-    "Side Plank",
-    "Bird Dog",
-  ]);
+  res.send(exercises);
 });
 
 app.get("/api/exercises/:id", (req, res) => {
-  res.send(req.params.id);
+  const exercise = exercises.find((e) => e.id == parseInt(req.params.id));
+  if (!exercise)
+    res.status(404).send("The exercise with the given ID was not found.");
+
+  res.send(exercise);
 });
 
 app.get("/api/workout/:year/:month", (req, res) => {
